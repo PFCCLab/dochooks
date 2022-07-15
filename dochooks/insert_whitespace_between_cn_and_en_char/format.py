cb4eaa4006ef98142a4cecb9870bbea66e3aac77
SigureMo <sigure.qaq@gmail.com>
2022-07-16 03:49:28 +0800

    ✨ feat: add hook `insert-whitespace-between-cn-and-en-char`

--- dochooks/insert_whitespace_between_cn_and_en_char/format.py
+++ dochooks/insert_whitespace_between_cn_and_en_char/format.py
@@ -6,22 +6,29 @@ from typing import Optional, Sequence
 from dochooks import __version__
 
 from ..utils.return_code import FAIL, PASS, ReturnCode
+from .check import check
 from .regex import REGEX_CN_WITH_EN, REGEX_EN_WITH_CN
 
 
-def check(string: str) -> bool:
-    if REGEX_CN_WITH_EN.search(string) or REGEX_EN_WITH_CN.search(string):
-        return True
-    return False
+def format(text: str) -> str:
+    text = REGEX_CN_WITH_EN.sub(r"\g<cn> \g<en>", text)
+    text = REGEX_EN_WITH_CN.sub(r"\g<en> \g<cn>", text)
+    return text
 
 
-def _check_file(file_path: str) -> ReturnCode:
+def _format_file(file_path: str) -> ReturnCode:
     return_code = PASS
+    formatted_text = ""
     with open(file_path, "r", encoding="utf8", newline="\n") as f:
         for lineno, line in enumerate(f, 1):
             if check(line):
-                print(f"No spaces between EN and CN chars detected at: {file_path}:{lineno}:\t{line}")
+                line = format(line)
                 return_code = FAIL
+                print(f"Add spaces between EN and CN chars in: {file_path}:{lineno}:\t{line}")
+            formatted_text += line
+    if return_code != PASS:
+        with open(file_path, "w", encoding="utf8", newline="\n") as f:
+            f.write(formatted_text)
     return return_code
 
 
@@ -33,7 +40,12 @@ def main(argv: Optional[Sequence[str]] = None) -> ReturnCode:
 
     ret_code: ReturnCode = PASS
     for filename in args.filenames:
-        ret_code |= _check_file(filename)
+        ret_code |= _format_file(filename)
+
+    if ret_code != PASS:
+        print("")
+        print("Snuged en and cn chars have been separated by a space. Now aborting the commit.")
+        print('You can check the changes made. Then simply "git add --update ." and re-commit')
     return ret_code
 
 
