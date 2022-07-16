e71c9b76fb6ac1ade8d2416f2c796356fa94bcd0
SigureMo <sigure.qaq@gmail.com>
2022-07-16 19:29:13 +0800

    ✅ test: add some tests

---
+++ tests/test_insert_whitespace_between_cn_and_en_char.py/test_check_and_format.py
@@ -0,0 +1,60 @@
+from dochooks.insert_whitespace_between_cn_and_en_char.format import format
+from dochooks.insert_whitespace_between_cn_and_en_char.check import check
+import pytest
+
+from typing import Final
+
+NEED_FORMAT: Final[bool] = True
+NEEDNT_FORMAT: Final[bool] = False
+
+cases: list[tuple[bool, str, str]] = [
+    (NEEDNT_FORMAT, "\n", "\n"),
+    (NEEDNT_FORMAT, "", ""),
+    (NEEDNT_FORMAT, "中文", "中文"),
+    (NEED_FORMAT, "中文a", "中文 a"),
+    (NEEDNT_FORMAT, "中文\na", "中文\na"),
+    (NEED_FORMAT, "中a文", "中 a 文"),
+    (
+        NEED_FORMAT,
+        "这是一行，a line\r\nthe next line呀！\n没了a",
+        "这是一行，a line\r\nthe next line 呀！\n没了 a",
+    ),
+    (
+        NEEDNT_FORMAT,
+        r"由于没有语法分析，因此并不能将这个与[Python](python.org)分开，公式$\sqrt{\frac{1}{n}}$也是一样",
+        r"由于没有语法分析，因此并不能将这个与[Python](python.org)分开，公式$\sqrt{\frac{1}{n}}$也是一样",
+    ),
+    (
+        NEEDNT_FORMAT,
+        "不会做额外的事情\r\n比如将在文末插入换行符",
+        "不会做额外的事情\r\n比如将在文末插入换行符",
+    ),
+    (
+        NEED_FORMAT,
+        """
+    这是一段长文本，会混杂一些英文
+    啦啦啦，char喵喵喵en啊啦啦啦。xxxx
+    en，char。
+    """,
+        """
+    这是一段长文本，会混杂一些英文
+    啦啦啦，char 喵喵喵 en 啊啦啦啦。xxxx
+    en，char。
+    """,
+    ),
+]
+
+
+@pytest.mark.parametrize("need_format, unformatted, formatted", cases)
+def test_check_and_format(need_format: bool, unformatted: str, formatted: str):
+
+    if need_format:
+        assert check(unformatted)
+    else:
+        assert unformatted == formatted
+        assert not check(unformatted)
+
+    assert format(unformatted) == formatted
+    # Test format process is stable
+    assert not check(formatted)
+    assert format(formatted) == formatted
---
+++ tests/test_insert_whitespace_between_cn_and_en_char.py/test_regex.py
@@ -0,0 +1,40 @@
+import re
+
+from dochooks.insert_whitespace_between_cn_and_en_char.regex import (
+    REGEX_CN_CHAR_STR,
+    REGEX_CN_WITH_EN,
+    REGEX_EN_CHAR_STR,
+    REGEX_EN_WITH_CN,
+)
+
+
+def test_pure_cn_chars_pattern():
+    assert re.search(REGEX_CN_CHAR_STR, "纯中文") is not None
+    assert re.search(REGEX_EN_CHAR_STR, "纯中文") is None
+    assert re.search(REGEX_CN_CHAR_STR, "，") is None
+    assert REGEX_CN_WITH_EN.search("纯中文") is None
+    assert REGEX_EN_WITH_CN.search("纯中文") is None
+
+
+def test_pure_en_chars_pattern():
+    assert re.search(REGEX_EN_CHAR_STR, "Pure English") is not None
+    assert re.search(REGEX_CN_CHAR_STR, "Pure English") is None
+    assert REGEX_CN_WITH_EN.search("Pure English") is None
+    assert REGEX_EN_WITH_CN.search("Pure English") is None
+
+
+def test_cn_with_en_pattern():
+    assert REGEX_CN_WITH_EN.search("中文后面带English") is not None
+    assert REGEX_CN_WITH_EN.sub(r"\g<cn> \g<en>", "中文后面带English") == "中文后面带 English"
+    assert REGEX_CN_WITH_EN.sub(r"\g<cn> \g<en>", "中文后面带English啦") == "中文后面带 English啦"
+
+
+def test_en_with_cn_pattern():
+    assert REGEX_EN_WITH_CN.search("English with中文字符") is not None
+    assert REGEX_EN_WITH_CN.sub(r"\g<en> \g<cn>", "English with中文字符") == "English with 中文字符"
+    assert REGEX_EN_WITH_CN.sub(r"\g<en> \g<cn>", "呀English with 中文字符") == "呀English with 中文字符"
+
+
+def test_cn_with_punctuation_pattern():
+    assert REGEX_CN_WITH_EN.sub(r"\g<cn> \g<en>", "带上标点符号试试，en。") == "带上标点符号试试，en。"
+    assert REGEX_EN_WITH_CN.sub(r"\g<en> \g<cn>", "带上标点符号试试，en。") == "带上标点符号试试，en。"
