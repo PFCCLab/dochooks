40307dbd99d00a5b3ac3fa3ebb2bfcdc1ecbe9bb
SigureMo <sigure.qaq@gmail.com>
2024-10-13 22:35:51 +0800

    🔖 release: v0.5.0

--- dochooks/__init__.py
+++ dochooks/__init__.py
@@ -1,7 +1,7 @@
 # Meta information for the project.
 from __future__ import annotations
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __author__ = "Nyakku Shigure"
 __year__ = "2022"
 __project_info__ = {
